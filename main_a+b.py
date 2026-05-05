0409b4a682e2574be984139d9ca96554d3945f3b
Ahror <126563834+Alicricher@users.noreply.github.com>
2026-05-05 21:01:10 +0500

    Fozl idi nafig
    ez

--- main_a+b.py
+++ main_a+b.py
@@ -3,12 +3,4 @@ b=int(input("give number to b = "))
 c=str(input("give operation (+,-,*,/)"))
 if c=="+":
     print(a+b)
-elif c=="-":
-    print(a-b)
-elif c=="*":
-    print(a*b)
-elif c=="/":
-    if b!=0 or a!=0:
-        print(a/b)
-else:
-    print("yobnulsya shtoli")
+
